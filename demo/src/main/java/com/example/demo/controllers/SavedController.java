f25d8a83121b75490adad918808af31d6dd444ff
evan <evga0904@gmail.com>
2025-05-01 01:59:39 -0400

    Added saved and profile page along with controllers for both

--- demo/src/main/java/com/example/demo/controllers/SavedController.java
+++ demo/src/main/java/com/example/demo/controllers/SavedController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.example.demo.models.Comment;
+import com.example.demo.models.MovieThumbnail;
 import com.example.demo.models.Review;
 
 @Controller
@@ -22,8 +23,20 @@ public class SavedController {
     public ModelAndView page() {
         ModelAndView saved = new ModelAndView("savedPage");
 
+        String pageTitle = "Saved Movies";
+
+        saved.addObject("pageTitle", pageTitle);
+
         //Be sure to set has comments to false here
-        
+        MovieThumbnail thumbnail1 = new MovieThumbnail("1", "Pulp Fiction", "1994", "https://m.media-amazon.com/images/M/MV5BYTViYTE3ZGQtNDBlMC00ZTAyLTkyODMtZGRiZDg0MjA2YThkXkEyXkFqcGc@._V1_SX300.jpg",4.32);
+        MovieThumbnail thumbnail2 = new MovieThumbnail("2", "Pulp Fiction", "1994", "https://m.media-amazon.com/images/M/MV5BYTViYTE3ZGQtNDBlMC00ZTAyLTkyODMtZGRiZDg0MjA2YThkXkEyXkFqcGc@._V1_SX300.jpg",4.32);
+        MovieThumbnail thumbnail3 = new MovieThumbnail("3", "Pulp Fiction", "1994", "https://m.media-amazon.com/images/M/MV5BYTViYTE3ZGQtNDBlMC00ZTAyLTkyODMtZGRiZDg0MjA2YThkXkEyXkFqcGc@._V1_SX300.jpg",4.32);
+        List<MovieThumbnail> thumbnailList = new ArrayList<>();
+        thumbnailList.add(thumbnail1);
+        thumbnailList.add(thumbnail2);
+        thumbnailList.add(thumbnail3);
+
+        saved.addObject("thumbnail", thumbnailList);
 
         return saved;
     }
