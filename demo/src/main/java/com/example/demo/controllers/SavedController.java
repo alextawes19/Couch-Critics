7b31a1899fcc2c05d10cc40c7feb3666278027b2
Alex Tawes <alextawes8@gmail.com>
2025-05-01 09:54:50 -0400

    Saved service, controller, and routing setup

--- demo/src/main/java/com/example/demo/controllers/SavedController.java
+++ demo/src/main/java/com/example/demo/controllers/SavedController.java
@@ -3,6 +3,7 @@ package com.example.demo.controllers;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,10 +14,22 @@ import org.springframework.web.servlet.ModelAndView;
 
 import com.example.demo.models.Comment;
 import com.example.demo.models.Review;
+import com.example.demo.models.Movie;
+import com.example.demo.services.SavedService;
+import com.example.demo.services.UserService;
 
 @Controller
-@RequestMapping("/savedMovies")
+@RequestMapping("/saved")
 public class SavedController {
+
+    private final SavedService savedService;
+    private final UserService userService;
+
+    @Autowired
+    public SavedController(SavedService savedService, UserService userService) {
+        this.savedService = savedService;
+        this.userService = userService;
+    }
 
     @GetMapping
     public ModelAndView page() {
@@ -24,6 +37,21 @@ public class SavedController {
 
         //Be sure to set has comments to false here
         
+        try {
+            // Render saved movies for current user
+            String currentUserId = userService.getLoggedInUser().getUserId();
+            List<Movie> movies = savedService.getSavedMovies(currentUserId);
+            saved.addObject("movies", movies);
+
+            // No content message
+            if (movies.isEmpty()) {
+                saved.addObject("isNoContent", true);
+            }
+        } catch (Exception e) {
+            // If an error occured
+            String errorMessage = "Some error occured!";
+            saved.addObject("errorMessage", errorMessage);
+        }
 
         return saved;
     }
